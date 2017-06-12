2b34d1c7c9f88c2d25c2d2ab6bc8f5b16ab0b2e1
CannelleRb <cannelle.ruschbonte@gmail.com>
2017-06-12 20:10:54 +0200

    [Server CORS] added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 let express = require('express');
 let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
+let cors = require('cors');
 let User = require('./model/users')
 
 let app = express();
@@ -10,19 +11,13 @@ let router = express.Router();
 
 let port = process.env.API_PORT || 3001;
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121212.mlab.com:21212/everyday_life');
+//mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121212.mlab.com:21212/everyday_life');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
+app.use(cors());
+app.options('*',cors());
 
 router.get('/', function(req, res) {
     res.json({ message: 'API initialised'});
@@ -31,5 +26,5 @@ router.get('/', function(req, res) {
 app.use('/api', router);
 
 app.listen(port, function() {
-    console.log('Api running on port ${port}');
+    console.log('Api running on port '+port);
 });
