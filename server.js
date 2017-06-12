4666847655f55b68193aa31b26e58b3242d56f95
CannelleRb <cannelle.ruschbonte@gmail.com>
2017-06-12 23:22:36 +0200

    [Mongo] set up post and get requests from db

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ router.get('/', function(req, res) {
     res.json({ message: 'API initialised'});
 });
 
+router.route('/users')
+    .get(function(req,res){
+        User.find(function(error, users){
+            if (error) { res.send(error) };
+            res.json(users)
+        });
+    })
+    .post(function(req,res) {
+        var user = new User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+
+        user.save(function (error) {
+            if (error) {
+                res.send(error);
+            }
+            res.json({message: 'User successfully added !'});
+        });
+    });
+
 app.use('/api', router);
 
 app.listen(port, function() {
