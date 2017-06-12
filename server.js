47539658cfdc2bfc4d3b23a79554032ed2d1f32a
CannelleRb <cannelle.ruschbonte@gmail.com>
2017-06-12 20:13:44 +0200

    [Gitignore] ignored .idea folder

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ let router = express.Router();
 
 let port = process.env.API_PORT || 3001;
 
-//mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121212.mlab.com:21212/everyday_life');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121212.mlab.com:21212/everyday_life');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
